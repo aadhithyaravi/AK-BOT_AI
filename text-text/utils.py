0d06576b67ed44a34084c37a5316e5f2d8fd2f11
Aadhithya <93704356+aadhithyaravi@users.noreply.github.com>
2025-09-27 13:36:49 +0530

    Delete text-text directory

--- text-text/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-def load_json(filepath: str):
-        path=os.path.join((os.path.dirname(__file__)),"..","assets",filepath)
-        path = os.path.abspath(path) 
-        with open(path, "r",encoding='utf-8') as f:
-             raw_text=json.load(f)
-        return raw_text
